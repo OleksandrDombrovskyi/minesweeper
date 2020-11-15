0959de79a0295cfb0ef150dce202f55737021be4
Dombrovskyi <doleksandr@playtika.com>
2020-11-16 00:08:05 +0200

    Updates

---
+++ precache-manifest.1fea3f17645acee0f667caa57f7186e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecc4db5eaa9321f4e6fa0b5ed52afb2",
+    "url": "/sapper/index.html"
+  },
+  {
+    "revision": "8666ca1646ffb2c69bb3",
+    "url": "/sapper/static/css/main.8ea19c49.chunk.css"
+  },
+  {
+    "revision": "2e20fc33614a889dbbf5",
+    "url": "/sapper/static/js/2.c1c060b5.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/sapper/static/js/2.c1c060b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8666ca1646ffb2c69bb3",
+    "url": "/sapper/static/js/main.d1e48d0a.chunk.js"
+  },
+  {
+    "revision": "78897189c700bbe0a312",
+    "url": "/sapper/static/js/runtime-main.b63fdf8d.js"
+  },
+  {
+    "revision": "582cac299faac1ab8d378a6488ba2ced",
+    "url": "/sapper/static/media/Digital_Dismay.582cac29.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21122779554fa41e61dc5b779c0647b2.js"
+  "/sapper/precache-manifest.1fea3f17645acee0f667caa57f7186e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sapper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
