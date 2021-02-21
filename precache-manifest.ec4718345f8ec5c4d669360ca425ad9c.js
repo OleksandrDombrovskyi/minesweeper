c4246a8c42fc907c0100ea4566138ec24c927f45
Dombrovskyi <doleksandr@playtika.com>
2021-02-22 00:06:02 +0200

    Updates

---
+++ precache-manifest.ec4718345f8ec5c4d669360ca425ad9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e602569ef09b44c02231cbdf7bc2bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de470a9045b56c2f38c",
+    "url": "/static/css/main.12944813.chunk.css"
+  },
+  {
+    "revision": "405a1498a1c50d821348",
+    "url": "/static/js/2.286b2f2b.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/static/js/2.286b2f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de470a9045b56c2f38c",
+    "url": "/static/js/main.8ec71816.chunk.js"
+  },
+  {
+    "revision": "396c969a4f48f4fcea07",
+    "url": "/static/js/runtime-main.78db9cce.js"
+  },
+  {
+    "revision": "582cac299faac1ab8d378a6488ba2ced",
+    "url": "/static/media/Digital_Dismay.582cac29.otf"
+  }
+]);
