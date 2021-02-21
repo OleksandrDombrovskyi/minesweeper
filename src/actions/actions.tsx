5b2fd2bd9c288fda7914c6a54d9e7c268c69fe60
Dombrovskyi <doleksandr@playtika.com>
2021-02-21 14:45:06 +0200

    Changed url to minesweeper

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -4,37 +4,21 @@ import {LevelParams} from "../reducers/game/data";
 import {createAction} from "typesafe-actions";
 
 export const startGame = createAction('startGame')<LevelParams>();
-
 export const cellClicked = createAction('cellClicked')<CellPosition>();
-
 export const cellRightClicked = createAction('cellRightClicked')<CellPosition>();
-
 export const cellClickFailed = createAction('cellClickFailed')<void>();
-
 export const gameWon = createAction('gameWon')<void>();
-
 export const incrementTime = createAction('incrementTime')<void>();
-
 export const dragNDropFlag = createAction('dragNDropFlag')<MoveFlagProps>();
-
 export const selectFlag = createAction('selectFlag')<void>();
-
 export const selectCrossedFlag = createAction('selectCrossedFlag')<void>();
-
 export const selectQuestion = createAction('selectQuestion')<void>();
-
 export const selectCrossedQuestion = createAction('selectCrossedQuestion')<void>();
-
 export const selectMagicWand = createAction('selectMagicWand')<void>();
-
 export const openMenuAction = createAction('openMenuAction')<void>();
-
 export const closeMenuAction = createAction('closeMenuAction')<void>();
-
 export const openLevelDialog = createAction('openLevelDialog')<void>();
-
 export const closeLevelDialog = createAction('closeLevelDialog')<void>();
-
 export const changeLevel = createAction('changeLevel')<GameLevel>();
 
 export interface MoveFlagProps {
